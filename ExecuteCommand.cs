a994015b39265e9ef2aa5c72002a05a8ded474f5
Gregor Zeman <if19b210@technikum-wien.at>
2021-03-08 17:33:42 +0100

    Color Change On Button Click

---
+++ ExecuteCommand.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace TourPlaner
+{
+    public class ExecuteCommand : ICommand
+    {
+        private readonly MainViewModel _mainViewModel;
+        public ExecuteCommand(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+            _mainViewModel.PropertyChanged += (sender, args) =>
+            {
+                Debug.Print("command: reveived prop changed");
+                if (args.PropertyName == "Input")
+                {
+                    Debug.Print("command: reveived prop changed of Input");
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            Debug.Print("command: can execute");
+            return !string.IsNullOrWhiteSpace(_mainViewModel.Input);
+        }
+
+        public void Execute(object parameter)
+        {
+            Random rn = new Random();
+            Debug.Print("command: execute");
+            _mainViewModel.Output = $"Hello {_mainViewModel.Input}, nice to see you!";
+            // mw.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0, 0));
+            Application.Current.MainWindow.Background = PickBrush();
+            ColorAnimation ca = new ColorAnimation(Colors.Blue, new Duration(TimeSpan.FromSeconds(4)));
+            Application.Current.MainWindow.Background = new SolidColorBrush(Colors.Red);
+            Application.Current.MainWindow.Background.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+            _mainViewModel.Input = string.Empty;
+            Debug.Print("command: execute done");
+        }
+
+        private Brush PickBrush()
+        {
+            Brush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            int random = rnd.Next(properties.Length);
+            result = (Brush)properties[random].GetValue(null, null);
+
+            return result;
+        }
+
+
+
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -48,15 +48,5 @@ namespace TourPlaner
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
